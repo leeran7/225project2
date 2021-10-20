4280c5586a7c1f3304758ec954c616de46320da8
Leeran Farin <leeran7@gmail.com>
2021-10-20 03:36:29 -0400

    almost finished listing page, added productInfo page with query to send data, started implementing add to cart feature

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-$("a").hover(
-    function(){
-        $(this).toggleClass('active')
-    }
-)
-$("product").ready(function() {
-    $('.hover').on('touchstart touchend', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('hoverEffect');
-    });
+$(".product-link").click(function(){
+    let href = $(this).attr("href");
+    href += "?pic=";
+    const src = $(this).find(".card-img").attr("src");
+    href += src;
+    href += "&title=";
+    let title = $(this).contents().find("h5").text();
+    href += title.replace(/\s/g, '-');
+    href += "&desc=";
+    let desc = $(this).contents().find(".card-text").first().text();
+    href += desc.replace(/\s/g, '-');
+    $(this).attr("href", href);
+});
+$(".cart-link").click(function(){
+    let href = $(this).attr("href");
+    href += "?pic=";
+    const src = $(".productInfo").find("#image").attr("src");
+    href += src;
+    href += "&title=";
+    let title = $(".productInfo").contents().find("#title").text();
+    href += title.replace(/\s/g, '-');
+    $(this).attr("href", href);
+
+
+    // let item = 
+    // $("cart-item").
+
+    //started implementing functionality to add items into the cart
 });
